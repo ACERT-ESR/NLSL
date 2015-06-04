5971c5bf5aebfb1451a9dc0e238e433ad60413e2
John Franck <jmf356@cornell.edu>
2015-06-04 16:52:25 -0400

    edited runexample so it compares against the reference spc

--- runexample.py
+++ runexample.py
@@ -34,34 +34,38 @@ if __name__ == "__main__":
     pynlsl.nlsinit()
     run_file(open(filename_base+'.run'))
     for thisdatafile in data_files_out:
-        data = read_column_data(thisdatafile+'.spc')
-        fields = data[:,0]
-        experimental = data[:,1]
-        fit = data[:,2]
-        integral_of_spectrum = cumsum(experimental)
-        normalization = abs(sum(integral_of_spectrum))
         fig = figure(figsize = (9,6))
         fig.add_axes([0.1,0.1,0.6,0.8]) # l b w h
-        if data.shape[1] > 3:
-            components = data[:,3:]
-        else:
-            components = None
-        plot(fields,experimental/normalization,'k',linewidth = 1,label = 'experimental')
-        plot(fields,fit/normalization,'r',alpha = 0.5,linewidth = 2,label = 'fit ('+thisdatafile+'.spc)')
-        max_of_fit = max(fit)/normalization
-        if components is not None:
-            plot(fields,components/normalization,alpha = 0.3,linewidth = 1,label = 'component')
-        ax = gca()
-        ylims = ax.get_ylim()
-        scale_integral = max_of_fit/max(integral_of_spectrum)
-        plot(fields,integral_of_spectrum * scale_integral,'k:',alpha = 0.5,linewidth = 1,label = '$\int \int$ (scaled by %0.2g)'%scale_integral)
-        legend(bbox_to_anchor=(1.05,0,0.5,1), # bounding box l b w h
-                loc = 2, # upper left (of the bounding box)
-                borderaxespad=0.)
-        ax.set_ylim(ylims)
-        rms = mean((fit/normalization-experimental/normalization)**2)
-        ax.text(0.75, 0.75, 'rms = %0.2g'%rms,
-                horizontalalignment='left',
-                verticalalignment='top',
-                transform=ax.transAxes)
+        def show_spc(filename,linestyles = ['k','r'],show_components = True):
+            data = read_column_data(filename)
+            fields = data[:,0]
+            experimental = data[:,1]
+            fit = data[:,2]
+            integral_of_spectrum = cumsum(experimental)
+            normalization = abs(sum(integral_of_spectrum))
+            if data.shape[1] > 3:
+                components = data[:,3:]
+            else:
+                components = None
+            plot(fields,experimental/normalization,linestyles[0],linewidth = 1,label = 'experimental')
+            plot(fields,fit/normalization,linestyles[1],alpha = 0.5,linewidth = 2,label = 'fit ('+filename+')')
+            max_of_fit = max(fit)/normalization
+            if components is not None and show_components:
+                plot(fields,components/normalization,alpha = 0.3,linewidth = 1,label = 'component')
+            ax = gca()
+            ylims = ax.get_ylim()
+            scale_integral = max_of_fit/max(integral_of_spectrum)
+            plot(fields,integral_of_spectrum * scale_integral,'k:',alpha = 0.5,linewidth = 1,label = '$\int \int$ (scaled by %0.2g)'%scale_integral)
+            legend(bbox_to_anchor=(1.05,0,0.5,1), # bounding box l b w h
+                    loc = 2, # upper left (of the bounding box)
+                    borderaxespad=0.)
+            ax.set_ylim(ylims)
+            if show_components:# if showing the components, also show rms
+                rms = mean((fit/normalization-experimental/normalization)**2)
+                ax.text(0.75, 0.75, 'rms = %0.2g'%rms,
+                        horizontalalignment='left',
+                        verticalalignment='top',
+                        transform=ax.transAxes)
+        show_spc(thisdatafile+'.spc')
+        show_spc(thisdatafile+'.spc_ref',linestyles = ['g--','b--'],show_components = False)
     show()
