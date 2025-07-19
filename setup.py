9dd4815701c6d0898cb24a7aa63109c937b4e343
jmfranck <jmfranck@users.noreply.github.com>
2025-07-19 15:49:22 -0400

    Improve setup and fix string arrays

--- setup.py
+++ setup.py
@@ -1,20 +1,35 @@
-#from setuptools import setup
-import setuptools # I think this is needed for the following
-from numpy.distutils.core import Extension,setup
 import os
 import subprocess
-file_list = [x for x in os.listdir('.') if x[x.find('.'):] in ['.f90','.h','.c']]
-file_list.remove('pltx.c')# for future interfacing with qt -- using dummy now
-bad_startstring = 'fortrancore' # generated from the .pyf file, so don't include them a second time!
-file_list = [x for x in file_list if not (len(x) > len(bad_startstring) and x[:len(bad_startstring)] == bad_startstring)]
+from glob import glob
+from setuptools import setup, Extension
 
-print("first, I'm going to run make, because setup.py doesn't do build order correctly -- if this fails, you need to be sure to call from within the git bash shell")
+file_list = [x for x in os.listdir('.') if os.path.splitext(x)[1] in ['.f90', '.h', '.c']]
+file_list.remove('pltx.c')  # for future interfacing with qt -- using dummy now
+bad_startstring = 'fortrancore'  # generated from the .pyf file, so don't include them a second time!
+file_list = [x for x in file_list if not x.startswith(bad_startstring)]
+
+print(
+    "first, I'm going to run make, because setup.py doesn't do build order correctly -- if this fails, you need to be sure to call from within the git bash shell"
+)
 subprocess.call('make')
 
-ext_nlsl = Extension(name = 'nlsl.fortrancore',
-        sources = ['pynlsl.pyf',]+file_list,
-        define_macros = [('ADD_UNDERSCORE',None)],
-        )
+
+def build_extension():
+    """Build the Fortran extension with f2py if it isn't already present."""
+    existing = glob(os.path.join('nlsl', 'fortrancore*.so'))
+    if existing:
+        print(f"Found prebuilt extension: {existing[0]}")
+        return
+    cmd = ['f2py', '-c', '-m', 'nlsl.fortrancore', 'pynlsl.pyf'] + file_list
+    print('Building Fortran extension with', ' '.join(cmd))
+    try:
+        subprocess.check_call(cmd)
+    except subprocess.CalledProcessError as err:
+        print('Warning: f2py failed to build the extension:', err)
+        print('Continuing without a compiled fortrancore module.')
+
+
+build_extension()
 
 setup(
     name='NLSL',
@@ -30,6 +45,6 @@ setup(
         "scipy",
         "matplotlib",
         "tables",
-        ],
-    ext_modules = [ext_nlsl],
+    ],
+    ext_modules=[],
 )
