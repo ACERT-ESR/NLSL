ebba8a05b8e1b0376d24a3dac3ed8daca3cb6e19
jmfranck <jmfranck@users.noreply.github.com>
2025-08-09 09:56:44 -0400

    Reset working and data arrays on initialization

--- src/fortran/nlsl.f90
+++ src/fortran/nlsl.f90
@@ -423,6 +423,7 @@
       use basis
       use lmcom
       use iterat
+      use parsav
       use stdio
 !
 !      implicit none
@@ -450,7 +451,7 @@
 !     Initialize parameter arrays
 !----------------------------------------
       do j=1,MXSITE
-         do i=1,NVPRM
+         do i=1,NFPRM
             fparm(i,j)=0.0d0
             ixx(i,j)=0
          end do
@@ -490,6 +491,50 @@
 
       do i=1,MXSPC
          serval(i)=0.0d0
+      end do
+
+!--------------------------------------------------
+!  Reset saved-parameter storage
+!--------------------------------------------------
+      xsaved=.false.
+      prsaved=.false.
+      nstsav=0
+      nspsav=0
+      nprsav=0
+      fxsave = 0.0d0
+      fprsav = 0.0d0
+      spsave = 0.0d0
+      ixsave = 0
+      iprsav = 0
+      ixxsav = 0
+      tagsav = ' '
+
+!--------------------------------------------------
+!  Reset variable-parameter bookkeeping arrays
+!--------------------------------------------------
+      do i=1,MXVAR
+         prmax(i)=0.0d0
+         prmin(i)=0.0d0
+         prscl(i)=0.0d0
+         xfdstp(i)=0.0d0
+         ibnd(i)=0
+         ixpr(i)=0
+         ixst(i)=0
+      end do
+      do i=1,MXJCOL
+         xerr(i)=0.0d0
+         tag(i)=" "
+      end do
+      mtxclc=.false.
+!
+!--------------------------------------------------
+!  Reset working parameter copies
+!--------------------------------------------------
+      do i=1,NFPRM
+         fepr(i)=0.0d0
+      end do
+      do i=1,NIPRM
+         iepr(i)=0
       end do
 !
 !-------------------------------------------------------
@@ -533,16 +578,33 @@
          sphs(i)=0.0d0
          slb(i)=0.0d0
          srng(i)=0.0d0
-         sbi(i)=0.0d0
          iform(i)=0
          ibase(i)=0
+         nft(i)=0
+         nrmlz(i)=0
          npts(i)=0
          ishft(i)=0
          ixsp(i)=1
          idrv(i)=1
+         dataid(i)=' '
+         wndoid(i)=' '
       end do
       ishglb=0
+      shftflg=0
+      normflg=0
       written=0
+      inform=0
+      bcmode=0
+      drmode=0
+      data   = 0.0d0
+      spltmp = 0.0d0
+      rmsn   = 0.0d0
+
+!------------------------------------------------------------
+!  Clear stored spectra
+!------------------------------------------------------------
+      spectr = 0.0d0
+      wspec  = 0.0d0
 !
 !----------------------------------------
 !  -- Enable autoscaling for all sites
@@ -571,6 +633,13 @@
       iwflag=1
       confid=0.683
       ctol=1.0d-3
+      njcol=0
+      itridg=0
+      iitrfl=0
+      jacobi=0
+      ixp1p=0
+      ixs1p=0
+      output=0
 !
 !--------------------------------------------------
 !  -- Set initial values for line search parameters
