cb32204c5ecd9da03411c8147abd6475885821f6
John Franck <jmfranck@jmfDebian.syr.edu>
2025-11-07 21:57:54 -0500

    cleaned up changes

--- src/fortran/core/lpnam.f90
+++ src/fortran/core/lpnam.f90
@@ -24,35 +24,32 @@
 !  Note that blanks are specified for parameters that are not 
 !  intended to be user-accessible, but must still be passed to EPRLS
 !
-      integer, parameter, private :: parnam_strlen_value = 6
       integer, save :: parnam_strlen
-      data parnam_strlen /parnam_strlen_value/
+      data parnam_strlen /6/
       character*6 parnam(NFPRM)
       save parnam
       data parnam /'PHASE ', 'GIB0  ', 'GIB2  ', 'WXX   ', 'WYY   ',
      #            'WZZ   ', 'GXX   ', 'GYY   ', 'GZZ   ', 'AXX   ',
-     #            'AYY   ', 'AZZ   ', 'RX    ', 'RY    ', 'RZ    ',
+     #            'AYY   ', 'AZZ   ', 'RX    ', 'RY    ', 'RZ    ', 
      #            'PML   ', 'PMXY  ', 'PMZZ  ', 'DJF   ', 'DJFPRP',
-     #            'OSS   ', 'PSI   ', 'ALPHAD', 'BETAD ', 'GAMMAD',
+     #            'OSS   ', 'PSI   ', 'ALPHAD', 'BETAD ', 'GAMMAD',  
      #            'ALPHAM', 'BETAM ', 'GAMMAM', 'C20   ', 'C22   ',
      #            'C40   ', 'C42   ', 'C44   ', 'LB    ', 'DC20  ',
      #            'B0    ', 'GAMMAN', 'CGTOL ', 'SHIFTR', 'SHIFTI',
      #            'RANGE ', '      ', '      ' /
 !
-      integer, parameter, private :: iprnam_strlen_value = 6
       integer, save :: iprnam_strlen
-      data iprnam_strlen /iprnam_strlen_value/
+      data iprnam_strlen /6/
       character*6 iprnam(NIPRM)
       save iprnam
       data iprnam /'IN2   ', 'IPDF  ', 'IST   ', 'ML    ', 'MXY   ',
-     #            'MZZ   ', 'LEMX  ', 'LOMX  ', 'KMN   ', 'KMX   ',
+     #            'MZZ   ', 'LEMX  ', 'LOMX  ', 'KMN   ', 'KMX   ',   
      #            'MMN   ', 'MMX   ', 'IPNMX ', 'NORT  ', 'NSTEP ',
      #            'NFIELD', 'IDERIV', '      ', '      ', '      ',
      #            '      ', '      ', '      ', '      ' /
 !
-      integer, parameter, private :: alias_strlen_value = 6
       integer, save :: alias_strlen
-      data alias_strlen /alias_strlen_value/
+      data alias_strlen /6/
       character*6 alias1(NALIAS)
       save alias1
       data alias1 /  'W1    ', 'W2    ', 'W3    ', 
@@ -67,15 +64,13 @@
      #              'APRP  ', '      ', 'APLL  ',
      #              'RPRP  ', '      ', 'RPLL  ' /
 !
-      integer, parameter, private :: symstr_strlen_value = 10
       integer, save :: symstr_strlen
-      data symstr_strlen /symstr_strlen_value/
+      data symstr_strlen /10/
       character*10, dimension(NSYMTR), save, target ::
      # symstr = (/'CARTESIAN ', 'SPHERICAL ', 'AXIAL     '/)
 !
-      integer, parameter, private :: symbol_strlen_value = 10
       integer, save :: symbol_strlen
-      data symbol_strlen /symbol_strlen_value/
+      data symbol_strlen /10/
       character*10 symbol(NSYMBL)
       save symbol
       data symbol /'BROWNIAN  ', 'NONBROWNIA', 'ANISOVISCO',
